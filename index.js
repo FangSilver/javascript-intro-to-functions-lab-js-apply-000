80653767bb6e5128da42a7df19f3bb9ff905bba4
lively-bundle-0725 <tbandachit@gmail.com>
2020-02-10 02:53:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ function logShout(string){
 }
 
 function logWhisper(string){
-  console.log(string.toUpperCase);
+  console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma(string){
-
+if(string
 }
