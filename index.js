bb56beaa5018e080aa39ccb64d30047c8c6df8cf
lively-bundle-0725 <tbandachit@gmail.com>
2020-02-10 02:44:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ function shout(string){
   return string.toUpperCase()
 }
 
+function whisper(string){
+  return string.toLowerCase()
+}
+
 function shout(string){
-  return string.topperCase()
+  return string.toUpperCase()
 }
