c50fa106ce99879ea138586e32e8ffb40d37370d
lively-bundle-0725 <tbandachit@gmail.com>
2020-02-10 02:56:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-if(string.toLowerCase() == lowercase)
-  return "I can't hear you!"
+if(string.toLowerCase() ==   return "I can't hear you!"
 if(string.toUpperCase() == upercase)
   return "YES INDEED!"
 if(string == "I love you")
